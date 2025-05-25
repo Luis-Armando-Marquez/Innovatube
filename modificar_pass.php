1a9e9a8914adbad397a2b3ed6e0c5ed1c2b90e48
Luis-Armando-Marquez <luis_armando_mc@outlook.com>
2025-05-25 14:24:56 -0600

    Solucion de errores

--- modificar_pass.php
+++ modificar_pass.php
@@ -1,11 +1,6 @@
 <?php
     ob_start();
     session_start();
-    if (!isset($_SESSION['Usuario_recuperar'])) {
-
-        header("Location: recuperar.php");
-        exit();
-    }
 
     $Usuario = $_SESSION['Usuario_recuperar'];
 
