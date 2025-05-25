f71fbb9fc1dbbe0023cd8d8b135891c1c75c913f
Luis-Armando-Marquez <luis_armando_mc@outlook.com>
2025-05-25 12:03:51 -0600

    Pagina de navegacion
    Se crea la pagina de navegación para ver los videos, dando la bienvenida al usuario con su nombre loggueado y creando el archivo de cierre de sesión

---
+++ navegacion.php
@@ -0,0 +1,43 @@
+<!-- CONEXION_BASE_DATOS -->
+ <?php
+    session_start();
+    if (!isset($_SESSION['Usuario_innova'])){
+        header("Location: index.php");
+        exit();
+    }
+
+    //CONEXION A DB
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $dbname = "innovatube";
+
+    $conn = new mysqli($host, $user, $password, $dbname);
+?>
+<!-- END_DB -->
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>InnovaTube</title>
+    <link rel="stylesheet" href="style_navegacion.css">
+</head>
+<body>
+    <!-- HEADER -->
+    <header class="Header">
+        <div class="Header_div">
+            <a href="#"><img src="Castor.png" class="Header_logo"></a>
+            <!-- ENLACES -->
+            <nav class="Header_nav">
+                <ul class="Header_nav_ul">
+                  <li class="Header_nav_ul">Bienvenido, <?php echo htmlspecialchars($_SESSION['Usuario_innova']); ?></li>
+                  <li class="Header_nav_ul"><a href="cerrar.php">Cerrar sesión</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <!-- END HEADER -->
+</body>
+</html>
